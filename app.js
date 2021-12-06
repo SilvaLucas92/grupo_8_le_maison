8b2b01a36bfd7f0956b267c2cd63563cdaf0eb69
Dir18 <dirlyzandrea30@gmail.com>
2021-12-06 09:15:49 -0300

    Modifique el archivo app.js

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const app = express();
 const path = require ('path');
 
 app.listen(8000,() =>
-    console.log('servidor corriendo en el puerto 8000'));
+    console.log('Servidor corriendo en el puerto 8000'));
 app.get('/', (req, res) =>{
-    let htmlPath = path.resolve (__dirname, 'views/home.html');
+    let htmlPath = path.resolve (__dirname, '/views/home.html');
     res.sendFile(htmlPath);
 })
