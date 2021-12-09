4b1963063863e1d6af83f97c7b7a61d2cd241e09
SilvaLucas92 <l.silva8692@gmail.com>
2021-12-09 20:28:00 -0300

    home

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ const path = require ('path');
 
 app.listen(8000,() =>
     console.log('Servidor corriendo en el puerto 8000'));
+
+app.use(express.static(path.resolve(__dirname, "public")));
+
 app.get('/', (req, res) =>{
-    let htmlPath = path.resolve (__dirname, '/views/home.html');
+    let htmlPath = path.resolve (__dirname, 'views/home.html');
     res.sendFile(htmlPath);
 })
