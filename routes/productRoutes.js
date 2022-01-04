885086ebe5cbfd80ae1d2cde5982289d281ac321
SilvaLucas92 <l.silva8692@gmail.com>
2022-01-04 19:31:29 -0300

    division carpeta views

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,6 +1,5 @@
 const express = require ("express");
 const router = express.Router();
-<<<<<<< HEAD
 const controller = require("../controller/productController.js")
 
 
@@ -12,14 +11,6 @@ router.get("/detail", controller.detail);
 router.get('/new', controller.create)
 //modProduct
 router.get('/mod', controller.mod)
-=======
-const controller = require("../controller/productController")
 
-
-//product cart
-router.get("/productCart", controller.cart );
-//productDetail
-router.get("/productDetail", controller.detail);
->>>>>>> de60347ce0bbbf73dc6eec5d634c490e4d94f864
 
 module.exports = router;
