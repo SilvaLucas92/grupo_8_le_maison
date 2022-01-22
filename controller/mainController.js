165d2c255a528c8bebeda3085232bfd040a438fc
SilvaLucas92 <l.silva8692@gmail.com>
2022-01-22 18:25:32 -0300

    agregue prod

--- controller/mainController.js
+++ controller/mainController.js
@@ -1,46 +1,4 @@
-const productList = [
-    {
-        name: 'Escritorio',
-        description: 'Moderno y práctico escritorio, con estilo vintage, inspirador de comodidad y confort, Fabricado con madera de cedro.',
-        price: '$8900',
-        size: 'con medidas: Alto: 70cm, Ancho: 50cm, Profundidad: 40cm.',
-    },
-    {
-        name: 'Espejo',
-        description: 'espejito',
-        price: '$4800',
-        size: 'con medidas: Alto: 70cm, Ancho: 120cm.' ,
-        
-    },
-    {
-        name: 'Biblioteca',
-        description: 'kyufly',
-        price: '$15000',
-        size: 'con medidas: Alto:  , Ancho:  , Profundidad:  .',
-        
-    },
-    {
-        name: 'Sillon',
-        description: 'jhdfgsuhdofi',
-        price: '$32000',
-        size: 'con medidas: Alto:  , Ancho:  , Profundidad:  .',
-        
-    },
-    {
-        name: 'Acolchado',
-        description: 'jhdfgsuhdofi',
-        price: '$3900',
-        size: 'con medidas: Alto:  , Ancho:  , Profundidad:  .',
-        
-    },
-    {
-        name: 'Mesa Ratona',
-        description: 'jhdfgsuhdofi',
-        price: '$24300',
-        size: 'con medidas: Alto:  , Ancho:  , Profundidad:  .',
-        
-    }
-];
+
 
 const controller = {
     home: (req, res) => {
