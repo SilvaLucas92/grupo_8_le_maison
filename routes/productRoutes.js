7d0c64d9d626fdbfc85761a90293d3a1e6ee1be0
dir18 <dirlyzandrea30@gmail.com>
2022-01-22 18:25:03 -0300

    Crud de product

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,16 +1,30 @@
 const express = require ("express");
 const router = express.Router();
-const controller = require("../controller/productController.js")
+const controller = require("../controller/productController")
 
+
+
+//productList
+router.get("/", controller.browse);
 
 //product cart
-router.get("/cart", controller.cart );
-//productDetail
-router.get("/detail", controller.detail);
+router.get("/cart", controller.cart);
+
 //newProduct
-router.get('/new', controller.create)
+router.get('/new', controller.create);
+//POST
+router.post("/", controller.add);
+
 //modProduct
-router.get('/mod', controller.mod)
+router.get('/mod', controller.mod);
+//PUT
+router.put("/mod/:id", controller.update);
+
+//productDetail
+router.get("/:id", controller.detail);
+
+//DELETE
+router.delete("/:id", controller.delete);
 
 
 module.exports = router;
