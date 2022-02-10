2fd36e499636e95c49406c08910e2145cbd98fb2
dir18 <dirlyzandre30>
2022-02-10 16:18:39 -0300

    Modularizacion de multer

--- middlewares/multerMD.js
+++ middlewares/multerMD.js
@@ -5,12 +5,19 @@ const storage = multer.diskStorage({
     destination: (req, file, cb)=> {
         let folder = path.join(__dirname, '../public/images')
         cb(null, folder);
+        
+        cb(null, path.join(__dirname, '../public/images'))
     },
     filename: (req, file, cb) => {      
         const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
         cb(null, 'img - ' + uniqueSuffix + '-' + file.fieldname + path.extname(file.originalname))
+        
+        const newFilname = 'img-' + Date.now() + path.extname(file.originalname);
+        cb(null, newFilname)
     }
 });
 const fileUpload = multer({storage});
+const upload = multer ({ storage });
 
 module.exports = fileUpload;
+module.exports = upload;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,30 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const multer = require ("multer");
-const path = require ("path");
 const {body} = require ("express-validator");
 const controller = require("../controller/userController.js")
+const upload = require('../middlewares/multerMD');
 
 // Express Validator
-
 const validateUser = [
     body ('name') .notEmpty().withMessage('Campo Obligatorio').bail(),
     body ('lastName') .notEmpty().withMessage('Campo Obligatorio').bail(),
     body ('email') .isEmail().withMessage('Campo Obligatorio').bail(),
     body ('password') .isLength({min:10}).withMessage('Campo Obligatorio').bail(),
 ]
-//Configuración Multer
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, path.join(__dirname, '../public/images'))
-    },
-    filename: (req, file, cb) => {
-        const newFilname = 'img-' + Date.now() + path.extname(file.originalname);
-        cb(null, newFilname)
-    }
-});
-
-const upload = multer ({ storage });
 
 //login
 router.get("/login", controller.login);
