b6a411610ece21c132765148957cd38690faba8d
SilvaLucas92 <l.silva8692@gmail.com>
2022-03-26 18:46:26 -0300

    cmabios en el css y boton del password

---
+++ public/js/loginValidation.js
@@ -0,0 +1,19 @@
+window.addEventListener('load', () => {
+    const passwordInput = document.querySelector('input[name=password]');
+    iconPassword.addEventListener('click', () => {
+        if (passwordInput.type == "password") { 
+            iconPassword.classList.remove("fa-eye-slash");
+            iconPassword.classList.add("fa-solid");
+            iconPassword.classList.add("fa-eye");
+            passwordInput.type = "text"
+        } else {
+            iconPassword.classList.remove("fa-solid");
+            iconPassword.classList.remove("fa-eye");
+            iconPassword.classList.add('fa-eye-slash');
+            passwordInput.type = "password"
+        }
+})})
+
+
+
+
