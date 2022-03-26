238583c8e48a5e18d6a2110c0528996798c28b41
SilvaLucas92 <l.silva8692@gmail.com>
2022-03-26 20:25:10 -0300

    ok

---
+++ public/js/productValidation.js
@@ -0,0 +1,8 @@
+window.addEventListener('load', () => {
+    const nameInput = document.querySelector('input[name=name]');
+    const descriptionInput = document.querySelector('input[name=description]');
+    const categoryInput = document.querySelector('select[name=category]');
+    const materialsInput = document.querySelector('select[name=materials]');
+    const colorsInput = document.querySelector('select[name=colors]');
+    const priceInput = document.querySelector('input[name=price]');
+})
