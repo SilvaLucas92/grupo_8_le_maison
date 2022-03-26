f1d2e68a16bbd2b06dca56e6f8cd1571e77bc098
SilvaLucas92 <l.silva8692@gmail.com>
2022-03-26 19:31:53 -0300

    cam

---
+++ public/js/registerValidation.js
@@ -0,0 +1,17 @@
+window.addEventListener('load', () => {
+    const passwordInput = document.querySelector('input[name=password]');
+    const iconPassword = document.querySelector('#iconPassword');
+
+    iconPassword.addEventListener('click', () => {
+        if (passwordInput.type == "password") { 
+            iconPassword.classList.remove("fa-eye-slash");
+            iconPassword.classList.add("fa-solid");
+            iconPassword.classList.add("fa-eye");
+            passwordInput.type = "text"
+        } else {
+            iconPassword.classList.remove("fa-solid");
+            iconPassword.classList.remove("fa-eye");
+            iconPassword.classList.add('fa-eye-slash');
+            passwordInput.type = "password"
+        }
+})})
