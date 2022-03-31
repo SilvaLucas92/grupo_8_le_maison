3847e779f0873cee8558bf0217821393be44e200
dir18 <dirlyzandrea30@gmail.com>
2022-03-31 13:04:42 -0300

    ok

--- middlewares/expressMD.js
+++ middlewares/expressMD.js
@@ -3,7 +3,7 @@
 const {body} = require ("express-validator");
 
 const userValidator = [
-    body ('name') .notEmpty().withMessage('Campo Obligatorio').bail(),
+    body ('name') .isLength({min:2}).withMessage('Campo Obligatorio . Debe tener minimo 2 caracteres') .notEmpty().bail(),
     body ('email') .isEmail().withMessage('Campo Obligatorio').bail(),
     body ('password') .isLength({min:6}).withMessage('La contraseña debe tener como minimo 6 caracteres').bail(),
 ];
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,6 +1,6 @@
 const express = require ("express");
 const router = express.Router();
-const controller = require("../controller/productController")
+const controller = require("../controller/productController");
 const fileUpload = require('../middlewares/multerMD');
 
 
